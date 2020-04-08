2dbe957f3daa7f709c5960e147c8af46cfecfb4d
Sung Bae <singsang2@gmail.com>
2020-04-08 19:05:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -106,10 +106,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     programmer_hash[:alan_kay][:languages].
-     
+     programmer_hash[:alan_kay][:languages]=alans_new_info
      programmer_hash
-     
 end
 
 def adding_to_dennis
@@ -128,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages].append("Assembly")
+     programmer_hash
 
 end
